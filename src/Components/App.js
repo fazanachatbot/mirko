a2470195ff7af7367445fafda9152d703aa5e0dd
fazanachatbot <fazanachatbot@gmail.com>
2019-08-11 21:38:11 +0200

    finished nav, moving on to chat component

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './Main';
+import NavBar from './NavBar';
+
+
+export default () => {
+  useEffect(() => {
+    document.body.style.background = 'whitesmoke';
+  }, []);
+  return (
+    <>
+      <Main />
+      <NavBar />
+    </>
+  );
+};
