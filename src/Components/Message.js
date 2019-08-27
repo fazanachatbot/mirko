51ee02b573aae980398ce4cad137b5128bca16d6
Matej <49211125+voiys@users.noreply.github.com>
2019-08-27 16:57:18 +0200

    Delete Message.js

--- src/Components/Message.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ({ sentByUser, children }) => (
-   <>
-      <div className={`message message${sentByUser ? 'User' : 'Bot'}`}>
-         {children}
-      </div>
-   </>
-);
